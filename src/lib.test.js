831e24ca8516a2b3dc73efdd9342332c79331bf9
Lubenets Slava <wenzelua@gmail.com>
2019-06-11 12:01:24 +0300

    id:86063 feat(tests): initial commit

--- src/lib.test.js
+++ src/lib.test.js
@@ -1,7 +1,23 @@
 import BrandgilityEmbeddedApi from './lib';
 
-test('instance should be an instance of BrandgilityEmbeddedApi', () => {
-  const brandgilityEmbeddedApi = new BrandgilityEmbeddedApi(window);
+test('callback should be called when appropriate action + key has been received', () => {
+  const callback = jest.fn();
+  const iframe = document.createElement('iframe');
+  const identityKey = '@BrandgilityEmbeddedApi';
+  const action = 'some action';
 
-  expect(brandgilityEmbeddedApi).toBeInstanceOf(BrandgilityEmbeddedApi);
+  document.body.append(iframe);
+
+  const brandgilityEmbeddedApi = new BrandgilityEmbeddedApi(iframe.contentWindow);
+
+  brandgilityEmbeddedApi.on(action, callback);
+
+  const messageEvent = new MessageEvent('message', {
+    data: { identityKey, action },
+    source: iframe.contentWindow,
+  });
+
+  window.dispatchEvent(messageEvent);
+
+  expect(callback).toHaveBeenCalled();
 });
